64ba424899b1e6dc17ca1d3f3b9f0d494e5480e1
Gopal <gopalkurup@gmail.com>
2020-02-06 11:40:19 +0530

    Added other brightness mappings

--- ascii.py
+++ ascii.py
@@ -12,6 +12,26 @@ def rgb2brightness_avg(im_arr):
             res[i][j] = (im_arr[i][j][0] + im_arr[i][j][1] + im_arr[i][j][2])//3
     return res
 
+def rgb2brightness_min_max(im_arr):
+    n_rows = im_arr.shape[0]
+    n_cols = im_arr.shape[1]
+    res = np.ones((n_rows,n_cols),dtype=int)
+    for i in range(0,n_rows):
+        for j in range(0,n_cols):
+            res[i][j] = (np.min(im_arr[i][j]) + np.max(im_arr[i][j]))//2
+    return res
+
+def rgb2brightness_luminosity(im_arr):
+    n_rows = im_arr.shape[0]
+    n_cols = im_arr.shape[1]
+    res = np.ones((n_rows,n_cols),dtype=int)
+    for i in range(0,n_rows):
+        for j in range(0,n_cols):
+            res[i][j] = (0.21*im_arr[i][j][0] + 0.72*im_arr[i][j][1] + 0.07*im_arr[i][j][2])
+    return res
+
+
+
 def print_vals(im_arr):
     n_rows = im_arr.shape[0]
     n_cols = im_arr.shape[1]
@@ -47,10 +67,12 @@ if __name__ == "__main__":
     print(img_ip.format,img_ip.size,img_ip.mode)
 
     img = img_ip.resize((400,300))
-    img = img.filter(ImageFilter.DETAIL)
+    #img = img.filter(ImageFilter.DETAIL)
     im_arr = np.array(img.getdata(),int).reshape(img.size[1],img.size[0],3)
 
     convt_im_array = rgb2brightness_avg(im_arr)
+    #convt_im_array = rgb2brightness_min_max(im_arr)
+    #convt_im_array = rgb2brightness_luminosity(im_arr)
     #print_vals(convt_im_array)
     map2ascii(convt_im_array)
     #print_vals(convt_im_array)
